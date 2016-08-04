6fd47af543df9b9e38bb118a4e93f4080436c865
Blake Contreras <shardsofsilver@gmail.com>
2016-08-04 14:01:51 -0700

    test[client] Update app and map tests

--- client/components/app.js
+++ client/components/app.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import TruckList from '../containers/TruckList';
 import GoogleMap from '../containers/GoogleMap';
 
+
 export default class App extends Component {
   render() {
     return (
--- test/components/index.spec.js
+++ test/components/index.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
+
+google = sinon.stub();
 import Index from '../../client/index';
-import App from '../../client/components/app';
 import {Provider} from 'react-redux';
 
 describe('<Index />', () => {
@@ -19,7 +21,7 @@ describe('<Index />', () => {
     expect(wrapper.find(Provider)).to.exist;
   })
   it('should render on the DOM', () => {
-    const wrapper = mount(<Index />);
+    const wrapper = shallow(<Index />);
     expect(document.getElementById('app').hasChildNodes()).to.be.true;
   })
 })
